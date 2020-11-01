1869e9bc58b4aa3924a511eb4bfe6d537a504bfb
ShawnVosburg <shawn.vosburg@mail.mcgill.ca>
2020-11-01 13:33:04 -0500

    Implemented K-Fold cross validation

--- src/DataManager/DataManager.py
+++ src/DataManager/DataManager.py
@@ -7,6 +7,8 @@ import matplotlib.image as mpimage
 import PreprocessingStrategy as pps
 import json
 import uuid
+import numpy as np
+from sklearn.model_selection import KFold,train_test_split
 
 DATA_PATH = str(pathlib.Path(__file__).parent.absolute()) + '\\..\\..\\data\\'
 RAWDATA_PATH = DATA_PATH + '\\raw\\train.csv'
@@ -14,14 +16,48 @@ PROCESSEDDATA_FOLDER = DATA_PATH + '\\processed\\'
 PROCESSED_FILENAME = 'processed.json'
 
 class DataManager:
-    def __init__(self, seed = None):
+    def __init__(self, seed = 0):
         self._df = None
         self._labels = None
         self.label_name = ""
         self._seed = seed
         self.cmds = []
 
+        self._train_indexes = None 
+        self._test_indexes = None
+    
+    # Handeling of common sub-indexing
+    @property
+    def train_indexes(self):
+        if(self._train_indexes is None): return self._df.index
+        else:                            return self._train_indexes
+    @train_indexes.setter
+    def train_indexes(self,val):
+        self._train_indexes = val
+    @property
+    def test_indexes(self):
+        if(self._test_indexes is None):  return []
+        else:                            return self._test_indexes
+    @test_indexes.setter
+    def train_indexes(self,val):
+        self._test_indexes = val
+
+    # Common datasubsets 
+    @property
+    def df_Train(self):
+        return self._df.loc[self.train_indexes]
+    @property
+    def df_Test(self):
+        return self._df.loc[self.test_indexes]
+    @property
+    def labels_Train(self):
+        return self._labels.loc[self.train_indexes]
+    @property
+    def labels_Test(self):
+        return self._labels.loc[self.test_indexes]
+
     def importAndPreprocess(self,label_name,filepath = RAWDATA_PATH, savepath = PROCESSEDDATA_FOLDER):
+        """ Checks if pre-processed data already saved. Loads pre-processed data if so. Else, generates preprocessed data. """
         #1. Check to see if data has already been preprocessed
          # Load already saved preprocessed data
         if(os.path.isfile(savepath + PROCESSED_FILENAME)):
@@ -85,13 +121,25 @@ class DataManager:
         out = {'method' : method, 'hyperparams':hyperparams}
         self.cmds.append(out)
 
-    def k_fold(self, k):
-        """Returns generator that produces training, validation and test sets"""
-        pass
+    def split_data(self,test_ratio = 0.1):
+        #Seperates dataset into train and test datasets
+        self._train_indexes, self._test_indexes = train_test_split(self._df.index, random_state = self._seed)
 
-    def getDataSubset(self, indexes):
-        """Returns a subset of the data that corresponds to the indexes passed as a parameter."""
-        pass
+    def k_fold(self, k):
+        """Returns generator that produces training, validation"""
+        #Splits data
+        if(self._train_indexes is None):    self.split_data(test_ratio=0.1)
+
+        #Creates KFold generator
+        kf = KFold(n_splits=k,shuffle=True,random_state=self._seed)
+        
+        #Create generator that spits out data and labels for train and validation datasets
+        for train_idxes, val_idxes in kf.split(self.df_Train.to_numpy(), self.labels_Train.to_numpy()):
+            train_data = self.df_Train.iloc[train_idxes]
+            val_data = self.df_Train.iloc[val_idxes]
+            train_labels = self.labels_Train.iloc[train_idxes]
+            val_labels = self.labels_Train.iloc[val_idxes]
+            yield  train_data, val_data, train_labels, val_labels
 
     def setSeed(self, seed):
         """Set the seed for the randomizer."""
@@ -107,3 +155,13 @@ if __name__ == '__main__':
 
     #Testing Importing and Preprocessing Data
     dm.importAndPreprocess(label_name = 'species')
+
+    #Spliting data into [Train & Validation] & [Test] datasets
+    dm.setSeed(0) #Important in order to always have same test set
+    dm.split_data(test_ratio=0.1)
+
+    #Getting K-fold datasets
+    for i, (X_train, X_val, Y_train, Y_val) in enumerate(dm.k_fold(k=10)):
+        print('K =', i)
+        print('\tAmount of Train observations:', len(X_train))
+        print('\tAmount of Validation observations:', len(X_val))
