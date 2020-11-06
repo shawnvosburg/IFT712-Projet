76c9e0548b712e176868331a7e398e3cf602cfd2
ShawnVosburg <shawn.vosburg@mail.mcgill.ca>
2020-11-06 18:50:38 -0500

    created PolynomialFeatures Preprocessing

---
+++ src/DataManager/PreprocessingStrategy/PolynomialFeatures.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import pandas as pd
+from PreprocessingStrategy import PreprocessingStrategy
+from sklearn.preprocessing import PolynomialFeatures as _PolynomialFeatures
+
+class PolynomialFeatures(PreprocessingStrategy):
+    def __init__(self,**kwargs):
+        super().__init__()
+        self._method = _PolynomialFeatures(**kwargs)
+        self.hyperparams = self._method.get_params()
+        
+    def preprocess(self, data):
+        """ Return the transformed data """
+        return pd.DataFrame(self._method.fit_transform(data), columns = self._method.get_feature_names(data.columns), index = data.index)
+
+    def jsonify(self):
+        out = super().jsonify()
+        out.update(**{'hyperparams':self.hyperparams})
+        return out
